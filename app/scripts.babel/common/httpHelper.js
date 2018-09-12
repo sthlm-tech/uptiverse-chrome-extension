0b179c34ab8440b7b721c5a7e6e02cb01ca303c8
Ulf Dahlstrand <ulf.dahlstrand@klarna.com>
2018-09-12 13:14:49 +0200

    Updated urls to new uptiverse page

--- app/scripts.babel/common/httpHelper.js
+++ app/scripts.babel/common/httpHelper.js
@@ -1,44 +1,47 @@
 var httpHelper = {};
 
-  httpHelper.post = function (url, data, callback){
-    httpHelper.parseAuthorizationCookie(function(authToken){
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.setRequestHeader('Authorization', authToken);
-      xhr.onreadystatechange = function() {//Call a function when the state changes.
-          if(xhr.readyState == 4 && xhr.status == 200) {
-              callback(JSON.parse(xhr.responseText));
-          }
+httpHelper.post = function(url, data, callback) {
+  httpHelper.parseAuthorizationCookie(function(authToken) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("Authorization", authToken);
+    xhr.onreadystatechange = function() {
+      //Call a function when the state changes.
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        callback(JSON.parse(xhr.responseText));
       }
-      xhr.send(JSON.stringify(data));
-    });
-  }
+    };
+    xhr.send(JSON.stringify(data));
+  });
+};
 
-  httpHelper.get = function (url, callback){
-    httpHelper.parseAuthorizationCookie(function(authToken){
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.setRequestHeader('Authorization', authToken);
-      xhr.onreadystatechange = function() {//Call a function when the state changes.
-          if(xhr.readyState == 4 && xhr.status == 200) {
-              callback(JSON.parse(xhr.responseText));
-          }
+httpHelper.get = function(url, callback) {
+  httpHelper.parseAuthorizationCookie(function(authToken) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("Authorization", authToken);
+    xhr.onreadystatechange = function() {
+      //Call a function when the state changes.
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        callback(JSON.parse(xhr.responseText));
       }
-      xhr.send();
-    });
-  }
+    };
+    xhr.send();
+  });
+};
 
-  httpHelper.parseAuthorizationCookie = function(callback){
-    const cookieDetails = { name: 'id_token', domain: 'herokuapp.com' };
-    chrome.cookies.getAll(cookieDetails, function(cookie) {
-        if (cookie.length === 0) {
-            return;
-        }
-        const authToken = 'JWT ' + cookie[0].value;
-        callback(authToken);
-    });
-  }
+httpHelper.parseAuthorizationCookie = function(callback) {
+  const cookieDetails = { name: "id_token", domain: "uptiverse.se" };
+  chrome.cookies.getAll(cookieDetails, function(cookie) {
+    if (cookie.length === 0) {
+      return;
+    }
+    const authToken = "JWT " + cookie[0].value;
+    console.log(authToken);
+    callback(authToken);
+  });
+};
 
 module.exports = httpHelper;
--- app/scripts.babel/services/recruitService.js
+++ app/scripts.babel/services/recruitService.js
@@ -1,41 +1,41 @@
-import * as httpHelper from './../common/httpHelper.js';
-import * as linkedInUserIdHelper from './../common/linkedInUserIdHelper.js';
+import * as httpHelper from "./../common/httpHelper.js";
+import * as linkedInUserIdHelper from "./../common/linkedInUserIdHelper.js";
 var service = {};
 var recruitIdCache = {};
 
 service.getRecruit = function(currentUrl, callback) {
-  var searchUrl = 'https://uptiverse-recruit.herokuapp.com/recruits/find';
+  var searchUrl = "https://uptiverse-recruits.herokuapp.com/recruits/find";
   var requestData = { link: currentUrl };
-  httpHelper.post(searchUrl, requestData, function(response){
+  httpHelper.post(searchUrl, requestData, function(response) {
     callback(response);
   });
-}
+};
 
 service.createRecruit = function(recruit, callback) {
-  var searchUrl = 'https://uptiverse-recruit.herokuapp.com/recruits/create';
+  var searchUrl = "https://uptiverse-recruits.herokuapp.com/recruits/create";
   var requestData = { recruit: recruit };
-  httpHelper.post(searchUrl, requestData, function(response){
+  httpHelper.post(searchUrl, requestData, function(response) {
     callback(response);
   });
-}
+};
 
 service.getRecruitId = function(currentUrl, callback) {
-  linkedInUserIdHelper.parse(currentUrl, function(profileId){
-    if(profileId){
-      if(recruitIdCache[profileId]){
+  linkedInUserIdHelper.parse(currentUrl, function(profileId) {
+    if (profileId) {
+      if (recruitIdCache[profileId]) {
         callback(recruitIdCache[profileId]);
-      }else{
-        service.getRecruit(currentUrl, function(response){
-          if(response.recruits.length > 0){
+      } else {
+        service.getRecruit(currentUrl, function(response) {
+          if (response.recruits.length > 0) {
             recruitIdCache[profileId] = response.recruits[0]._id;
             callback(response.recruits[0]._id);
-          }else{
+          } else {
             callback();
           }
         });
       }
     }
   });
-}
+};
 
 export default service;
